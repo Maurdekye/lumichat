27fe477645c3cf592b5934381d6733b8b65b70f8
Maurdekye <ncolaprete@gmail.com>
2024-02-15 18:36:40 -0500

    basic feedback for login form

--- front/src/main.rs
+++ front/src/main.rs
@@ -1,10 +1,20 @@
+use std::time::Duration;
+
 use yew::prelude::*;
 
 use crate::login::Login;
 
+use gloo_timers::future::TimeoutFuture as Timeout;
+
+fn sleep(duration: Duration) -> Timeout {
+    Timeout::new(duration.as_millis() as u32)
+}
+
 mod login {
 
     extern crate wasm_bindgen_futures as futures;
+
+    use std::time::Duration;
 
     use futures::wasm_bindgen::JsValue;
     use gloo_console::log;
@@ -16,17 +26,30 @@ mod login {
 
     use shared::login;
 
+    use super::sleep;
+
     #[derive(Serialize)]
     pub enum Msg {
         UpdateIdentifier(String),
         UpdatePassword(String),
-        Login,
+        Request,
+        Response(login::Response),
+        ClearMessage,
+        Reload,
+    }
+
+    #[derive(Clone)]
+    pub enum InfoMessage {
+        Success(String),
+        Progress(String),
+        Error(String),
     }
 
     #[derive(Clone)]
     pub struct Login {
         identifier: String,
         password: String,
+        message: Option<InfoMessage>,
     }
 
     impl Component for Login {
@@ -37,20 +60,22 @@ mod login {
             Login {
                 identifier: "".to_string(),
                 password: "".to_string(),
+                message: None,
             }
         }
 
-        fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+        fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
             log!("msg:", JsValue::from_serde(&msg).unwrap());
             match msg {
                 Msg::UpdateIdentifier(identifier) => self.identifier = identifier,
                 Msg::UpdatePassword(password) => self.password = password,
-                Msg::Login => {
+                Msg::Request => {
                     let Login {
                         identifier,
                         password,
+                        ..
                     } = self.clone();
-                    futures::spawn_local(async move {
+                    ctx.link().send_future(async {
                         log!("Sending login request");
                         let response: login::Response = Request::post("/login")
                             .json(&login::Request {
@@ -64,10 +89,36 @@ mod login {
                             .json()
                             .await
                             .expect("Unable to decode response");
-                        log!("LoginResponse:", JsValue::from_serde(&response).unwrap());
+                        log!("Login response:", JsValue::from_serde(&response).unwrap());
+                        Msg::Response(response)
                     });
-                    return false;
+                    self.message = Some(InfoMessage::Progress("Logging in...".to_string()))
                 }
+                Msg::Response(login::Response::Failure(failure_reason)) => {
+                    let message = match failure_reason {
+                        login::FailureReason::AlreadyLoggedIn => "Already logged in!".to_string(),
+                        login::FailureReason::InvalidPassword => "Invalid password".to_string(),
+                        login::FailureReason::UserDoesNotExist => {
+                            format!("User with username/email '{}' not found", self.identifier)
+                        }
+                    };
+                    self.message = Some(InfoMessage::Error(message));
+                    ctx.link().send_future(async {
+                        sleep(Duration::from_secs(5)).await;
+                        Msg::ClearMessage
+                    });
+                }
+                Msg::Response(login::Response::Success) => {
+                    self.message = Some(InfoMessage::Success("Logged in!".to_string()));
+                    ctx.link().send_future(async {
+                        sleep(Duration::from_secs(1)).await;
+                        Msg::Reload
+                    });
+                }
+                Msg::ClearMessage => {
+                    self.message = None;
+                }
+                Msg::Reload => {}
             };
             true
         }
@@ -97,8 +148,25 @@ mod login {
                                     Msg::UpdatePassword(input.value())
                                 })}
                             />
-                            <button onclick={ctx.link().callback(|_| Msg::Login)}>{"Login"}</button>
+                            <button onclick={ctx.link().callback(|_| Msg::Request)}>{"Login"}</button>
                         </div>
+                        {
+                            match &self.message {
+                                None => html! {},
+                                Some(InfoMessage::Success(msg)) =>
+                                    html! {
+                                        <div class={"login-message success"}>{msg}</div>
+                                    },
+                                Some(InfoMessage::Progress(msg)) =>
+                                    html! {
+                                        <div class={"login-message progress"}>{msg}</div>
+                                    },
+                                Some(InfoMessage::Error(msg)) =>
+                                    html! {
+                                        <div class={"login-message error"}>{msg}</div>
+                                    },
+                            }
+                        }
                     </div>
                 </div>
             }
