5dd138c9850f8600d5fc31a39229113a034f0c20
Maurdekye <ncolaprete@gmail.com>
2024-02-16 16:54:04 -0500

    added admin signup env var flag

--- src/main.rs
+++ src/main.rs
@@ -23,6 +23,8 @@ mod schema;
 pub type Pool = r2d2::Pool<ConnectionManager<PgConnection>>;
 pub type PoolConnection = r2d2::PooledConnection<ConnectionManager<PgConnection>>;
 
+struct AdminSignup(bool);
+
 fn validate_password(_password: &str) -> Result<(), PasswordValidationError> {
     Ok(()) // no validation for now
 }
@@ -70,10 +72,25 @@ pub struct NewUser<'a> {
     admin: bool,
 }
 
+#[post("/admin-signup")]
+async fn admin_signup_handler(
+    body: web::Json<create_user::Request>,
+    db: Data<Pool>,
+    admin_signup_enabled: Data<AdminSignup>,
+) -> impl Responder {
+    // check if admin signup is enabled
+    if !admin_signup_enabled.get_ref().0 {
+        return HttpResponse::Unauthorized().finish();
+    }
+
+    // proceed to user creation
+    let mut db = db.get().expect("Database not available");
+    create_user_inner(body, &mut db, true).await
+}
+
 #[post("/create-user")]
 async fn create_user_handler(
     identity: Identity,
-    request: HttpRequest,
     body: web::Json<create_user::Request>,
     db: Data<Pool>,
 ) -> impl Responder {
@@ -83,7 +100,16 @@ async fn create_user_handler(
     if !user.admin {
         return HttpResponse::Unauthorized().finish();
     }
+    
+    // proceed to user creation
+    create_user_inner(body, &mut db, false).await
+}
 
+async fn create_user_inner(
+    body: web::Json<create_user::Request>,
+    db: &mut PoolConnection,
+    make_admin: bool,
+) -> HttpResponse {
     // decode request
     let body = body.into_inner();
     println!("/create-user: {body:#?}");
@@ -99,7 +125,7 @@ async fn create_user_handler(
     let user_query = users
         .filter(username.eq(&body.username))
         .or_filter(email.eq(&body.email))
-        .first::<FullUser>(&mut db)
+        .first::<FullUser>(db)
         .optional()
         .expect("Error querying database");
 
@@ -113,17 +139,16 @@ async fn create_user_handler(
         username: &body.username,
         email: &body.email,
         password_hash: &encrypt_password(&body.username, &body.password),
-        admin: false,
+        admin: make_admin,
     };
 
     // Insert new user into the database
     let new_user: FullUser = insert_into(users)
         .values(&new_user)
-        .get_result(&mut db)
+        .get_result(db)
         .expect("Error inserting new user");
 
-    // Log user in
-    Identity::login(&request.extensions(), new_user.id.to_string()).unwrap();
+    // Respond successfully
     HttpResponse::Ok().json(create_user::Response::Success(new_user.into()))
 }
 
@@ -198,11 +223,13 @@ async fn main() -> std::io::Result<()> {
     let port = env::var("PORT").expect("PORT must be set");
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     let redis_url = env::var("REDIS_URL").unwrap_or("redis://127.0.0.1:6379".to_string());
+    let admin_signup = env::var("ADMIN_SIGNUP") == Ok("TRUE".to_string());
     let host_addr = format!("localhost:{}", port);
 
     println!("port: {port}");
     println!("database_url: {database_url}");
     println!("redis_url: {redis_url}");
+    println!("admin_signup: {admin_signup}");
 
     // setup db connection
     let manager = ConnectionManager::<PgConnection>::new(database_url);
@@ -220,6 +247,7 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(move || {
         App::new()
             .app_data(Data::new(pool.clone()))
+            .app_data(Data::new(AdminSignup(admin_signup)))
             .wrap(IdentityMiddleware::default())
             .wrap(SessionMiddleware::new(
                 redis.clone(),
